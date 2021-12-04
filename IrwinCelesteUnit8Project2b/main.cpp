2293f4dc9bb2f4f9e8f40fe28a8eaa2425d3c2e3
Celeste Irwin <celeste.irwin@sinclair.edu>
2021-12-04 00:13:15 -0500

    Built driver to test functionality

--- IrwinCelesteUnit8Project2b/main.cpp
+++ IrwinCelesteUnit8Project2b/main.cpp
@@ -27,6 +27,13 @@ int half(int number)
 
 int main(int argc, const char * argv[])
 {
+    double a= 7.0;
+    float b= 5.0f;
+    int c = 3;
+    
+    cout<<half(a) <<endl;
+    cout<<half(b) <<endl;
+    cout<<half(c) <<endl;
     
     return 0;
 }
