a736927544b71dc36053c9859ca9acf7029dbd0d
Celeste Irwin <celeste.irwin@sinclair.edu>
2021-12-03 23:38:41 -0500

    Blank program to begin project with

--- IrwinCelesteUnit8Project2b/main.cpp
+++ IrwinCelesteUnit8Project2b/main.cpp
@@ -4,11 +4,17 @@
 //
 //  Created by Celeste Irwin on 12/3/21.
 //
+//Celeste Irwin
+//CIS 1202 800
+//12-03-2021
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    
     return 0;
 }
